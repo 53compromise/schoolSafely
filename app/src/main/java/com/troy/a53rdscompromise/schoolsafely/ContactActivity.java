24560f369f86088f26282927de1debb2ee44d283
skyjung <skyjung4243@gmail.com>
2019-11-10 19:23:31 -0800

    sky pt. 3

--- app/src/main/java/com/troy/a53rdscompromise/schoolsafely/ContactActivity.java
+++ app/src/main/java/com/troy/a53rdscompromise/schoolsafely/ContactActivity.java
@@ -2,6 +2,7 @@ package com.troy.a53rdscompromise.schoolsafely;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        TextView contactName = (TextView)findViewById(R.id.contactName);
+        contactName.setText();
     }
 }
