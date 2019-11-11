c732e359d08fe0ef6d249c9a090d9189fb85b889
safegergis <safegerg@mail.com>
2019-11-10 19:24:10 -0800

    safe pt 1.

---
+++ app/src/main/java/com/troy/a53rdscompromise/schoolsafely/ProtocolActivity.java
@@ -0,0 +1,13 @@
+package com.troy.a53rdscompromise.schoolsafely;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProtocolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_protocol);
+    }
+}
