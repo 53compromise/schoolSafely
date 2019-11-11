17e46afab7d8b17cf6860e2dad02bf408b8fdc6e
safegergis <safegerg@mail.com>
2019-11-11 01:18:03 -0800

    fixed everything

--- app/src/main/java/com/troy/a53rdscompromise/schoolsafely/WeatherActivity.java
+++ app/src/main/java/com/troy/a53rdscompromise/schoolsafely/WeatherActivity.java
@@ -2,12 +2,22 @@ package com.troy.a53rdscompromise.schoolsafely;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
+
 
 public class WeatherActivity extends AppCompatActivity {
+    String cityName;
+    final String API = "5f46ba5e597f328209a63e7d39453ff9";
+    EditText cityText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        cityText = findViewById(R.id.cityText);
+        cityName = cityText.getText().toString();
     }
 }
